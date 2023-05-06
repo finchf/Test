b9a03a713cb2148f65584c5bbbd2ad7481ba44c8
Dima <dmbor123@gmail.com>
2023-05-06 19:02:33 +0300

    Test

---
+++ homework_lesson1.py
@@ -0,0 +1,24 @@
+age = input('Введи вік: ')
+age = int(age)
+name = input('Введіть ім`я: ')
+if 'a' in name:
+    print('Допобачення')
+else:
+    print('good, lets go')
+
+
+
+if age >= 100:
+    print('death')
+elif age >= 18:
+    print('ok')
+else:
+    print('No ok')
+
+if age % 2:
+    print('Не парне')
+else:
+    print('Парне')
+
+
+    
